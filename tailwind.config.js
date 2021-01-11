078f62767e2850b43f288ba33bc636897f619734
Manu Arora <mannuarora700@gmail.com>
2021-01-11 20:25:56 +0530

    🔩 EDIT: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-  purge: [],
+  purge: ["./pages/**/*.js", "./components/**/*.js"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
     fontFamily: {
-      inter: ['Inter'],
-      robotomono: ['Roboto Mono'],
+      inter: ["Inter"],
+      robotomono: ["Roboto Mono"],
     },
     letterSpacing: {
-      tightest: '-.075em',
+      tightest: "-.075em",
     },
   },
   variants: {
